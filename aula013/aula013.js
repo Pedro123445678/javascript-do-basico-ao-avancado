2fdcd80d2194c910aec91af04b6e2a9b992c6aec
Pedro123445678 <135380362+Pedro123445678@users.noreply.github.com>
2023-08-14 13:12:20 -0300

    atualizei

---
+++ aula013/aula013.js
@@ -0,0 +1,21 @@
+let energia=100
+let clima="chovendo"
+if(energia > 70 && clima=="sol"){
+    console.log("vou a praia")
+}else{
+    console.log("vou a cinema")
+}
+console.log("fim do programa")
+
+
+//let num=100
+
+//if(num>10){
+//console.log("Numeral maior que 10")
+//console.log("comando 2")
+//}else{
+  //  console.log("Numeral menor ou igual que 10")
+//}
+
+
+//console.log("fim do programa")
