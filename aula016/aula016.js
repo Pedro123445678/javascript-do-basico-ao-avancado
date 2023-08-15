20484d28d83a7c88baa1816841bd7020ea21a66b
Pedro123445678 <135380362+Pedro123445678@users.noreply.github.com>
2023-08-15 14:26:30 -0300

    atualizei

--- aula016/aula016.js
+++ aula016/aula016.js
@@ -1,7 +1,13 @@
+const objs= document.getElementsByTagName("div")
 let num=[10,20,30,40,50]
-for(n in num){
-    console.log(n)
+for(let i=0; i<num.length;i++){
+    console.log(num[i])
 }
-for(let i=0;i<num.length;i++){
-    console.log(i)
+for(o of objs){
+    console.log(o.innerHTML="Curso")
+   // console.log(o)
+}
+for(o in objs){
+    console.log(objs[o].innerHTML)
+   // console.log(o)
 }
