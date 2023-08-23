87c9357c2a6917e6664d64df7af8c2d679fd69f4
Pedro123445678 <135380362+Pedro123445678@users.noreply.github.com>
2023-08-23 14:59:32 -0300

    atualizei

---
+++ pedra,papel e tesoura/testando/345.js
@@ -0,0 +1,8 @@
+// este codigo imprime una janela no consola
+function printWindowWidth(){
+    console.log(window.innerWidth);
+}
+//Chama a Função para imprimir a largura da janela
+printWindowWidth();
+
+
---
+++ pedra,papel e tesoura/testando/banco de dado simples/cliente.js
@@ -0,0 +1,15 @@
+const clientes=[{
+ id:1,
+ nome:"Pedro Cardoso"
+ email:"Pedro Cardoso@exemplo.com ",
+ telefone:"11 9999-9999",
+ endereço: "Rua da Paz, 123, Centro, São Paulo,SP",
+},{
+id:2,
+nome: "Maria da Silva",
+email: "maria.silva@example.com",
+telefone:"11 9999-9998",
+endereço: "Rua da Felicidade, 456, Bairro dos Jardins, São Paulo, SP"
+},
+
+];
